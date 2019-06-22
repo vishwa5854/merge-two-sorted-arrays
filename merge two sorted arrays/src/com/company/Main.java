826187f7c15ccb2f3bc9099d7c071344ed2fcc9e
Vishwanath Bondugula <40670894+vishwa5854@users.noreply.github.com>
2019-06-22 22:40:39 +0530

    Add files via upload

---
+++ merge two sorted arrays/src/com/company/Main.java
@@ -0,0 +1,55 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter size of A");
+        int sizeOfA = scanner.nextInt();
+        System.out.println("enter size of B");
+        int sizeOfB = scanner.nextInt();
+        int[] a = new int[sizeOfA];
+        int[] b = new int[sizeOfB];
+        System.out.println("Enter into A:");
+        for(int d=0;d<sizeOfA;d++){
+            a[d] = scanner.nextInt();
+        }
+        System.out.println("Enter into B:");
+        for(int d=0;d<sizeOfB;d++){
+            b[d] = scanner.nextInt();
+        }
+        int[] c = new int[sizeOfA + sizeOfB];
+        int min;
+        if(sizeOfA > sizeOfB){
+            min = sizeOfB;
+        }
+        else {
+            min = sizeOfA;
+        }
+        int i=0,j=0,k,l=0;
+        for(k=0;k<min;k++){
+            if(a[i] < b[j]){
+                c[l++] = a[i++];
+            }
+            else {
+                c[l++] = b[j++];
+            }
+        }
+        if(i != sizeOfA){
+            for(;i<sizeOfA;i++){
+                c[l++] = a[i];
+            }
+        }
+        if(j != sizeOfB){
+            for(;j<sizeOfB;j++){
+                c[l++] = b[j];
+            }
+        }
+
+        for(int h=0;h<sizeOfA+sizeOfB;h++){
+            System.out.println(c[h]);
+        }
+    }
+}
